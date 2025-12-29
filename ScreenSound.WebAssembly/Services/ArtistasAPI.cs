75e6a6df465580bfb5c72909685815888d2c17e8
unknown <giovanna.vasques1101@hotmail.com>
2025-12-29 12:12:07 -0300

    feat: projeto WebAssembly, com service para comunicação com a API, e páginas para visualizar os artistas cadastrados e cadastrar novo artista

---
+++ ScreenSound.WebAssembly/Services/ArtistasAPI.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+using ScreenSound.Shared.Modelos.Requests;
+using ScreenSound.Shared.Modelos.Response;
+
+namespace ScreenSound.WebAssembly.Services;
+
+public class ArtistasAPI
+{
+    private readonly HttpClient _httpClient;
+    public ArtistasAPI(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+    public async Task<ICollection<ArtistaResponse>> GetArtistasAsync()
+    {
+       return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+    }
+
+    public async Task AddArtistaAsync(ArtistaRequest artista)
+    {
+        await _httpClient.PostAsJsonAsync("artistas", artista);
+    }
+}
